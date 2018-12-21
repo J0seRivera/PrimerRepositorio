efc350bd380e3c8a3ad43e40328a3f47e8c85676
Jose Riera <joseriveradl@gmail.com>
2018-12-20 19:20:24 -0600

    canvas con siclo y funciones

--- Script.js
+++ Script.js
@@ -1,9 +1,25 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf;
 
-lienzo.beginPath();
-lienzo.strokeStyle = "red";
-lienzo.moveTo(100, 100);
-lienzo.lineTo(200, 200);
-lienzo.stroke();
-lienzo.closePath();
+while (l < lineas)
+{
+  yi = 10 * l;
+  xf = 10 * (l +1);
+  dibujarLinea("red", 0 , yi, xf, 300);
+  l= l+2;
+
+}
+
+
+function dibujarLinea(color, xInicial, yinicial, xFinal, yFinal)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xInicial, yinicial);
+  lienzo.lineTo(xFinal, yFinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
